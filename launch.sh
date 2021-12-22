627ca54af07f03be0c635b3c2dd44000f6655786
Jason McLeod <jason@mcleods.me>
2021-12-22 11:24:36 -0500

    Fix 1.11.0 install, had bad version for actual run.

--- launch.sh
+++ launch.sh
@@ -6,9 +6,10 @@ cd /data
 
 
 if ! [[ -f serverinstall_91_2105 ]]; then
+	rm -f serverinstall_91* forge-*.jar
 	curl -o serverinstall_91_2105 https://api.modpacks.ch/public/modpack/91/2105/server/linux
 	chmod +x serverinstall_91_2105
-	./serverinstall_91_2093 --path /data --auto
+	./serverinstall_91_2105 --path /data --auto
 fi
 
 if [[ -n "$MOTD" ]]; then
@@ -25,5 +26,6 @@ if ! [[ "$EULA" = "false" ]]; then
 	echo "eula=true" > eula.txt
 fi
 
+FORGE_JAR=$(ls forge-*.jar)
 curl -o log4j2_112-116.xml https://launcher.mojang.com/v1/objects/02937d122c86ce73319ef9975b58896fc1b491d1/log4j2_112-116.xml
-java -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Dfml.queryResult=confirm -Dlog4j.configurationFile=log4j2_112-116.xml $JVM_OPTS -jar forge-1.16.5-36.2.9.jar nogui
+java -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Dfml.queryResult=confirm -Dlog4j.configurationFile=log4j2_112-116.xml $JVM_OPTS -jar $FORGE_JAR nogui
